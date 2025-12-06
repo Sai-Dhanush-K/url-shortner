d3696a0c87c06337f6e574ea38e218a4db4361e9
Sai Dhanush <kandulasrisaidhanush@gmail.com>
2025-12-06 16:15:04 +0530

    fixed the issues with create shortlink

--- short_links/serializers.py
+++ short_links/serializers.py
@@ -6,6 +6,9 @@ class ShortLinkSerializer(ModelSerializer):
         model=shortlink
         fields=["ID","code","Actual_URL","clicks","status","created_at","updated_at","expires_at"]
         read_only_fields=["ID","clicks","created_at","updated_at"]
-        def create(self, data):
-            data[expires_at]=data.get("expires_at",datetime.now()+timedelta(years=5))
-            return super().create(data)
+    def create(self, validated_data):
+        if not validated_data.get("expires_at",None):
+            print("heyyy")
+            validated_data["expires_at"]=datetime.now() + timedelta(days=365 * 5)
+        print(validated_data.get("expires_at",None))
+        return super().create(validated_data)
